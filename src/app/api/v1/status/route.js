4c9875d2e57fcffd3aa88ad5dd90958b9c602627
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-07-15 22:10:51 -0300

    feat: adds tryCatch with erro handling

--- src/app/api/v1/status/route.js
+++ src/app/api/v1/status/route.js
@@ -1,37 +1,49 @@
 import database from "../../../../../infra/database";
+import { InternalServerError } from "../../../../../infra/errors";
 
 export async function GET(request, response) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("SHOW SERVER_VERSION;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+    const databaseVersionResult = await database.query("SHOW SERVER_VERSION;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections"
-  );
+    const databaseMaxConnectionsResult = await database.query(
+      "SHOW max_connections"
+    );
 
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenedConnectionsResult = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
 
-  const databaseOpenedConnectionsValues =
-    databaseOpenedConnectionsResult.rows[0].count;
+    const databaseOpenedConnectionsValues =
+      databaseOpenedConnectionsResult.rows[0].count;
 
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
-  return Response.json(
-    {
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: databaseVersionValue,
-          max_connections: parseInt(databaseMaxConnectionsValue),
-          opened_connections: databaseOpenedConnectionsValues,
+    const databaseMaxConnectionsValue =
+      databaseMaxConnectionsResult.rows[0].max_connections;
+    return Response.json(
+      {
+        updated_at: updatedAt,
+        dependencies: {
+          database: {
+            version: databaseVersionValue,
+            max_connections: parseInt(databaseMaxConnectionsValue),
+            opened_connections: databaseOpenedConnectionsValues,
+          },
         },
       },
-    },
-    { status: 200 }
-  );
+      { status: 200 }
+    );
+  } catch (error) {
+    const internalServerError = new InternalServerError({
+      cause: error,
+      statusCode: 500,
+    });
+
+    return Response.json(internalServerError.toJSON(), {
+      status: internalServerError.statusCode,
+    });
+  }
 }
