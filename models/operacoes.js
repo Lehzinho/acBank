fb3096dcd837adcfb06cd45949d83b0f00decb21
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-07-18 12:06:31 -0300

    feat: adds new methods to accomodate new routes and refactore existing ones

--- models/operacoes.js
+++ models/operacoes.js
@@ -8,13 +8,14 @@ async function create({
   tipo,
   valor,
   descricao,
+  operacao_original_id = null,
 }) {
   const usuarioOrigem = await user.findOneByEmail(usuario_origem_email);
   const usuarioDestino = await user.findOneByEmail(
-    tipo === "TRANSFERENCIA" ? usuario_destino_email : usuario_origem_email
+    tipo !== "DEPOSITO" ? usuario_destino_email : usuario_origem_email
   );
 
-  if (tipo === "TRANSFERENCIA" && usuarioOrigem.saldo < valor) {
+  if (tipo !== "DEPOSITO" && usuarioOrigem.saldo < valor) {
     throw new ValidationError({
       message: "Fundos insuficientes.",
       action: "Verifique o saldo da conta.",
@@ -24,13 +25,20 @@ async function create({
   const results = await database.query({
     text: `
       INSERT INTO 
-      operacoes (usuario_origem_id, usuario_destino_id, tipo, valor, descricao)
+      operacoes (usuario_origem_id, usuario_destino_id, tipo, valor, descricao, operacao_original_id)
       VALUES 
-      ($1, $2, $3, $4, $5)
+      ($1, $2, $3, $4, $5, $6)
       RETURNING
       *
       ;`,
-    values: [usuarioOrigem.id, usuarioDestino.id, tipo, valor, descricao],
+    values: [
+      usuarioOrigem.id,
+      usuarioDestino.id,
+      tipo,
+      valor,
+      descricao,
+      operacao_original_id,
+    ],
   });
 
   const balance = await user.updateBalance(
@@ -41,6 +49,67 @@ async function create({
   );
 
   return { ...results.rows[0], ...balance };
+}
+
+async function reimbursement(id, novoTipo, descricao) {
+  const transaction = await getTransactionById(id);
+
+  const response = await database.query({
+    text: `
+        UPDATE operacoes 
+        SET 
+          tipo = $1,
+          revertida = $2
+        WHERE 
+          id = $3
+        RETURNING
+          *
+        ;`,
+    values: [novoTipo, true, transaction.id],
+  });
+
+  const operacao_original = response.rows[0];
+
+  // usuario_origem_email,
+  // usuario_destino_email,
+  // tipo,
+  // valor,
+  // descricao,
+  // operacao_original_id = null,
+  const usuarioOrigem = await user.findOneById(
+    operacao_original.usuario_origem_id
+  );
+  const usuarioDestino = await user.findOneById(
+    operacao_original.usuario_destino_id
+  );
+
+  const operacao_nova = await create({
+    usuario_origem_email: usuarioDestino.email,
+    usuario_destino_email: usuarioOrigem.email,
+    tipo: "REINBOLSO",
+    valor: operacao_original.valor,
+    descricao: descricao,
+    operacao_original_id: operacao_original.id,
+  });
+
+  return { operacao_nova: operacao_nova, operacao_original: operacao_original };
+}
+
+async function getTransactionById(id) {
+  const results = await database.query({
+    text: `
+        SELECT
+          *
+        FROM 
+          operacoes
+        WHERE
+          id = $1
+        LIMIT
+          1
+      ;`,
+    values: [id],
+  });
+  return results.rows[0]; // Retorna todas as transações, não apenas a primeira
 }
 
 async function getTransactionsByUserId(usuario) {
@@ -61,6 +130,8 @@ async function getTransactionsByUserId(usuario) {
 const operacoes = {
   create,
   getTransactionsByUserId,
+  reimbursement,
+  getTransactionById,
 };
 
 export default operacoes;
